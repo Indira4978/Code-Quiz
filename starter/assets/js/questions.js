01d5f87f74a007ed8ccc534a4841624fae276d43
Maho Yutopia <indira.salcede@gmail.com>
2023-12-31 18:44:39 +0000

    adds quizz questions and answers

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,80 @@
+// // questions
+// //What does HTML stand for?
+
+// What does HTML stand for?
+// a) Hyper Text Markup Language 
+// b) High-Level Text Management Language
+// c) Hyperlink and Text Management Language
+// d) Hyper Transfer Markup Language
+
+// Which of the following is used to apply styles to HTML elements?
+// a) JavaScript
+// b) CSS
+// c) HTML
+// d) Bootstrap
+
+// How do you declare a variable in JavaScript?
+// a) #variable
+// b) var variable;
+// c) variable := value;
+// d) variable() { }
+
+// What is Bootstrap primarily used for in web development?
+// a) Server-side scripting
+// b) Front-end framework for responsive design
+// c) Database management
+// d) Back-end development
+
+// Which tag is used to create a hyperlink in HTML?
+// a) <link>
+// b) <a>
+// c) <hlink>
+// d) <url>
+
+// How can you apply a style to a specific HTML element with CSS?
+// a) Using the class selector
+// b) Using the id selector
+// c) Using the tag selector
+// d) All of the above
+
+// What is the purpose of the addEventListener method in JavaScript?
+// a) To create a new element
+// b) To handle user actions like clicks and keypresses
+// c) To include an external JavaScript file
+// d) To define a function
+
+// Which class in Bootstrap is used to create a responsive navigation bar?
+// a) .navbar-toggle
+// b) .nav-link
+// c) .navbar-nav
+// d) .navbar-collapse
+
+// What does the "DOCTYPE" declaration in HTML do?
+// a) Defines the document type and version of HTML
+// b) Declares a new HTML element
+// c) Specifies the document title
+// d) Creates a hyperlink
+
+// How do you include an external CSS file in an HTML document?
+// a) <style> tag
+// b) <css> tag
+// c) <link> tag
+// d) <external> tag
+
+// Answers:
+
+// a) Hyper Text Markup Language
+// b) CSS
+// b) var variable;
+// b) Front-end framework for responsive design
+// b) <a>
+// d) All of the above
+// b) To handle user actions like clicks and keypresses
+// a) .navbar-toggle
+// a) Defines the document type and version of HTML
+// c) <link> tag
+
+
+
+
+
