a23a0c2b52c455a5de3edc6b8331f31f073e3e87
Maho Yutopia <indira.salcede@gmail.com>
2024-01-01 14:13:58 +0000

    adds logic pseudo code

---
+++ starter/assets/js/logic.js
@@ -0,0 +1,21 @@
+// when "start" button is clicked: 
+//displays first question  
+//starts the timer
+//--> event listener calls the function that we will define 
+
+// ***quiz function***//
+//displays questions with the answers in buttons 
+
+
+// use dom to access the question object
+// 
+//display the answer options in button
+
+//event listener checks for the option selected 
+
+// validate the answer 
+
+// if correct answer : display "correct" in green + correct answer sound
+// if wrong answer: display "wrong" + wrong answer sound
+// store score in local storage
+// display score
